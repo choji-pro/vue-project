71c188b235232ef0d83e0a880ea659332df1f1b6
Jason <seoulju.team@gmail.com>
2021-03-30 21:23:53 +0200

    added firebase

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,11 @@
 <template>
   <nav>
 
+    <v-snackbar v-model="snackbar" :timeout="4000" top color="success">
+      <span>Awesome! You added a new project.</span>
+      <v-btn flat color="white" @click="snackbar = false">Close</v-btn>
+    </v-snackbar>
+
     <v-toolbar flat app>
       <v-toolbar-side-icon @click="drawer = !drawer" class="grey--text"></v-toolbar-side-icon>
       <v-toolbar-title class="text-uppercase grey--text">
@@ -46,6 +51,9 @@
             The Net Ninja
           </p>
         </v-flex>
+        <v-flex class="mt-4 mb-3">
+          <Popup @projectAdded="snackbar=true" />
+        </v-flex>
       </v-layout>
       <v-list>
         <v-list-tile v-for="link in links" :key="link.text" router :to="link.route">
@@ -63,7 +71,11 @@
 </template>
 
 <script>
+import Popup from './Popup'
 export default {
+  components: {
+    Popup
+  },
   data() {
     return {
       drawer: false,
@@ -71,7 +83,8 @@ export default {
         { icon: 'dashboard', text: 'Dashboard', route: '/' },
         { icon: 'folder', text: 'My Projects', route: '/projects' },
         { icon: 'person', text: 'Team', route: '/team' },
-      ]
+      ],
+      snackbar: false
     }
   }
 }
